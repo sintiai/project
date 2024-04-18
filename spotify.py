001f5b18feb49629ab4e89c546a169952380de1b
Tajrene Begum <tajrene@Tajrenes-MacBook-Air.local>
2024-04-18 14:34:00 -0400

    edited

--- spotify.py
+++ spotify.py
@@ -2,6 +2,8 @@ import sqlite3
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
+
+
 client_id = "8c279de418d9430296bbb01fbabd6dd6"
 client_secret = "2d383ce0756348e5b96748c6d64aa270"
 client_credentials_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
