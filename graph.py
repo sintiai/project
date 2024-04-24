9fb39bcd5cb72653e4353a24568c57009b483bcf
sintiai <sintia@umich.edu>
2024-04-24 14:32:31 -0400

    added comments

--- graph.py
+++ graph.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#graph for calculate_average_popularity_by_artist
 def graph_spotify():
     df = pd.read_csv('spotify_calculations.csv')
     df.plot.bar(x='Artist Name', y='Average Rating', color='pink')
     plt.show()
 
+#graph for calculate_average_imdb_rating
 def graph_imdb():
     df = pd.read_csv('imdb_calculations.csv')
     df.plot.bar(x='Description', y='Average Rating', color='pink')
     plt.show()
-
+#graph for calculate_movie_count_by_genre
 def genre_imdb():
     df = pd.read_csv('genre_calculations.csv')
     df.plot.bar(x='Genre', y='Average Rating', color='blue')
@@ -18,8 +20,6 @@ def genre_imdb():
     plt.xlabel('Genre')
     plt.ylabel('Average Rating')
     plt.show()
-
-#def movie_count():
 
 
 def main():
